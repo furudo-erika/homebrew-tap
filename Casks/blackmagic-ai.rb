c39a50ea6f871d4004ca77d5f6ad5f1d057b595f
Lynn Mikami <192083063+furudo-erika@users.noreply.github.com>
2026-04-23 12:10:12 +0800

    blackmagic-ai 0.4.78

--- Casks/blackmagic-ai.rb
+++ Casks/blackmagic-ai.rb
@@ -1,14 +1,14 @@
 cask "blackmagic-ai" do
-  version "0.4.77"
+  version "0.4.78"
 
   on_arm do
-    sha256 "4626dbecf54be81baf020c3c15be7aada81453730138533f3316eea93dcd92c3"
+    sha256 "825c3102ce75562cf544d4486b46909462690002e0f8da62f5793dbb5794b8ea"
     url "https://pub-d259d1d2737843cb8bcb2b1ff98fc9c6.r2.dev/blackmagic-desktop/BlackMagic%20AI-#{version}-arm64.dmg",
         verified: "pub-d259d1d2737843cb8bcb2b1ff98fc9c6.r2.dev/blackmagic-desktop/"
   end
 
   on_intel do
-    sha256 "0a66de982916ca7c3760a6d9e656b9e0bc47d779d7160e9fec0281c0c5101fa0"
+    sha256 "2b73f756a3c9cc68052b1109962e73750545526cb6b670b176fe49e1f3b0417c"
     url "https://pub-d259d1d2737843cb8bcb2b1ff98fc9c6.r2.dev/blackmagic-desktop/BlackMagic%20AI-#{version}.dmg",
         verified: "pub-d259d1d2737843cb8bcb2b1ff98fc9c6.r2.dev/blackmagic-desktop/"
   end
